feca5c63d33076a20eb9db3c20eff28771384d29
ArtusAbelard <tribelha@gmail.com>
2024-03-09 22:15:41 +0100

    bgwhite removed

--- src/assets/Compoments/Cards.jsx
+++ src/assets/Compoments/Cards.jsx
@@ -20,7 +20,7 @@ export default function Cards(props) {
                 data.map((Element,id)=>{
                     return(
                         <div className="card w-96  shadow-xl rounded-xl overflow-hidden">
-                            <Link to={`/Details/${id}`}><figure className='h-[15rem] bg-slate-200'><img className='w-full h-[18rem] pt-5' src={data[id].flags.png} alt="Flag" /></figure></Link>
+                            <Link to={`/Details/${id}`}><figure className='h-[15rem]'><img className='w-full h-[18rem] pt-5' src={data[id].flags.png} alt="Flag" /></figure></Link>
                             <div className={`card-body ${darkMode?"text-white bg-[#2B3743]":"text-black bg-[#FFFFFF]"}`}>
                                 <h2 className="card-title text-2xl">{data[id].name.common}</h2>
                                 <p className=' text-xl'>Population : {data[id].population}</p>
